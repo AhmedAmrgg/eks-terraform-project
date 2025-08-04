715bde2e8fbfcbc9d736eaa6d8a4fc7b4bbc9763
amora2312001 <amora2312001@gmail.com>
2025-08-04 23:49:25 +0300

    test1

--- envs/pre-prod/main.tf
+++ envs/pre-prod/main.tf
@@ -1,26 +1,26 @@
 
-module EKS {
-  source = "../../modules/EKS"
+# module EKS {
+#   source = "../../modules/EKS"
  
-  environment = var.environment
-  cluster_name = var.cluster_name
-  aws_region = var.aws_region
-  subnet_ids   = module.Network.public_subnets  # ✅ الحل هنا
-  private_subnets = module.Network.private_subnets
-  public_subnets  = module.Network.public_subnets
+#   environment = var.environment
+#   cluster_name = var.cluster_name
+#   aws_region = var.aws_region
+#   subnet_ids   = module.Network.public_subnets  # ✅ الحل هنا
+#   private_subnets = module.Network.private_subnets
+#   public_subnets  = module.Network.public_subnets
   
-}
+# }
  
 
 
-module Network {
-  source = "../../modules/Network"
+# module Network {
+#   source = "../../modules/Network"
 
 
-  # azs = var.azs
-  vpc_cidr_block = var.vpc_cidr_block
-  vpc_availability_zones = var.vpc_availability_zones  # ✅
-  subnet_ids = module.Network.public_subnets
-# vpc
   
-}
+#   vpc_cidr_block = var.vpc_cidr_block
+#   vpc_availability_zones = var.vpc_availability_zones  # ✅
+#   subnet_ids = module.Network.public_subnets
+
+  
+# }
